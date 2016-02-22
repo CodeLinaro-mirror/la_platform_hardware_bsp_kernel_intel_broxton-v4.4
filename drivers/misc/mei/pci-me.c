d8dffb1414edf2e0960485d0e83eefda73080573
Alexander Usyskin <alexander.usyskin@intel.com>
2016-02-22 11:09:46 +0200

    FROMLIST: mei: recover after errors in runtime pm flow
    Schedule link reset if failed to perform runtime suspend or resume.
    Set active runtime pm stte on link reset
    to clean runtimr pm error, if present.
    
    Upstream-Status: Submitted[https://patchwork.kernel.org/patch/9181219/]
    Change-Id: If8f2d5e3d5b25fc35733c92889af72ff69bb4494
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

--- drivers/misc/mei/pci-me.c
+++ drivers/misc/mei/pci-me.c
@@ -396,6 +396,9 @@ static int mei_me_pm_runtime_suspend(struct device *device)
 
 	dev_dbg(&pdev->dev, "rpm: me: runtime suspend ret=%d\n", ret);
 
+	if (ret && ret != -EAGAIN)
+		schedule_work(&dev->reset_work);
+
 	return ret;
 }
 
@@ -418,6 +421,9 @@ static int mei_me_pm_runtime_resume(struct device *device)
 	mutex_unlock(&dev->device_lock);
 
 	dev_dbg(&pdev->dev, "rpm: me: runtime resume ret = %d\n", ret);
+
+	if (ret)
+		schedule_work(&dev->reset_work);
 
 	return ret;
 }
